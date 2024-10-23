957a85d4310cd2b95a7d0e16e88e7935e1106a3a
gsofron <giorgos.sofronas03@gmail.com>
2024-10-23 15:00:52 +0300

    init vamana() function implementation

--- include/vamana.hpp
+++ include/vamana.hpp
@@ -1,10 +1,15 @@
 #pragma once
 
+#include <limits>
+#include <random>
+
 #include "directed_graph.hpp"
+#include "greedy_search.hpp"
+#include "robust_prune.hpp"
 
 // Creates a random R-regular out-degree directed graph, using a vector of MathVectors
 template <typename T>
-DirectedGraph<T> *random_graph(std::vector<MathVector<T> *> vectors, int R) {
+DirectedGraph<T> *random_graph(std::vector<MathVector<T> *>& vectors, int R) {
     int vector_count = vectors.size(), index;
     // If there are k vertices, each vertex can have at most k-1 neighbours
     ERROR_EXIT(R > vector_count - 1, "Too many neighbors")
@@ -33,7 +38,7 @@ DirectedGraph<T> *random_graph(std::vector<MathVector<T> *> vectors, int R) {
 
 // Returns the medoid point of given dataset
 template <typename T>
-MathVector<T> *medoid(std::vector<MathVector<T> *> *vectors) {
+MathVector<T> *medoid(std::vector<MathVector<T> *>& vectors) {
     // Simple brute-force algorithm
     float min = std::numeric_limits<float>::max();
     MathVector<T> *v = nullptr;
@@ -49,4 +54,41 @@ MathVector<T> *medoid(std::vector<MathVector<T> *> *vectors) {
         }
     }
     return v;
+}
+
+// Vamana Indexing Algorithm implementation, based on provided paper
+template <typename T>
+DirectedGraph<T> *vamana(std::vector<MathVector<T> *> *P, float a, int L, int R) {
+    // Init the R-regular (counting out-degree only) graph
+    DirectedGraph<T> *G = random_graph(*P, R);
+    
+    // Init vectors' medoid
+    MathVector<T> *s = medoid(*P);
+    
+    // Create the random permutation sigma (σ)
+    std::vector<MathVector<T> *> sigma = *P;
+    // Source for how to shuffle a vector in C++: https://stackoverflow.com/a/6926473
+    auto rd = std::random_device {}; 
+    auto rng = std::default_random_engine { rd() };
+    std::shuffle(sigma.begin(), sigma.end(), rng);
+    
+    int n = P->size();
+    for (int i = 0; i < n; i++) {
+        auto [Lset, V] = GreedySearch(*G, s, sigma[i], 1, L);
+        robust_prune(G, sigma[i], V, a, R);
+
+        const auto& N_out_sigma_i = G->get_neighbors(sigma[i]);
+        for (auto j : N_out_sigma_i) {
+            const auto& N_out_j = G->get_neighbors(j);
+            if ((int)N_out_j.size() + 1 > R) {
+                auto new_N_out_j = N_out_j;
+                new_N_out_j.insert(sigma[i]);
+                robust_prune(G, j, new_N_out_j, a, R);
+            } else {
+                G->insert(j, sigma[i]);
+            }
+        }
+    }
+
+    return G;
 }
