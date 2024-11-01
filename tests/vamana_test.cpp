a3183621fe42bc50bc0555464bd07e9b74c40a76
gsofron <giorgos.sofronas03@gmail.com>
2024-11-01 20:29:57 +0200

    vamana write and read to and from file DONE with testing

--- tests/vamana_test.cpp
+++ tests/vamana_test.cpp
@@ -2,7 +2,7 @@
 #include "vamana.hpp"
 
 // Used for testing
-#define NUM_OF_ENTRIES 1000
+#define NUM_OF_ENTRIES 700
 #define A 1.1f
 #define L 20
 #define R 3
@@ -23,7 +23,44 @@ void test_vamana(void) {
     delete g;
 }
 
+void test_read_and_write_file(void) {
+    // Create a random vamana graph and test the read_from and write_to functions
+    Vectors<int> vectors = Vectors<int>(NUM_OF_ENTRIES, 0);
+    DirectedGraph *g1 = vamana(vectors, A, L, R);
+
+    const std::string file_name = "build/test_vamana_file";
+
+    // Write the graph to the file
+    write_vamana_to_file(*g1, file_name);
+
+    // Load the graph from the file into a new object
+    DirectedGraph *g2 = read_vamana_from_file(file_name);
+
+    // Check data coherence
+    int g1_size = g1->get_size(), g2_size = g2->get_size();
+    TEST_CHECK(g1_size == g2_size);
+    for (int i = 0; i < g1_size; i++) {
+        const auto& g1_neighbors = g1->get_neighbors(i);
+        int g1_n_size = g1_neighbors.size();
+
+        const auto& g2_neighbors = g2->get_neighbors(i);
+        int g2_n_size = g2_neighbors.size();
+
+        TEST_CHECK(g1_n_size == g2_n_size);
+
+        // Assure that same vertices have the same neighbors
+        for (auto it1 = g1_neighbors.begin(); it1 != g1_neighbors.end(); it1++) {
+            // Search needs to happen using find(), because the sets are unordered (hash tables)
+            TEST_CHECK(g2_neighbors.find(*it1) != g2_neighbors.end());
+        }
+    }
+
+    delete g1;
+    delete g2;
+}
+
 TEST_LIST = {
     { "test_vamana", test_vamana },
+    { "test_read_and_write_file", test_read_and_write_file },
     { NULL, NULL }
 };
