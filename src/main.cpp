e00255f27563d818e4c06c4e7a15e4a3d63e62c3
gzog23 <mix.raf.kos@gmail.com>
2024-11-13 02:30:05 +0200

    user can save vamana to a file using -s arguement

--- src/main.cpp
+++ src/main.cpp
@@ -17,23 +17,34 @@
 // Execution examples
 // time ./k23a -b siftsmall/siftsmall_base.fvecs -q siftsmall/siftsmall_query.fvecs -g siftsmall/siftsmall_groundtruth.ivecs -a 1.1 -l 150 -r 100
 // time ./k23a -b siftsmall/siftsmall_base.fvecs -q siftsmall/siftsmall_query.fvecs -g siftsmall/siftsmall_groundtruth.ivecs -v vamana.bin -a 1.1 -l 150 -r 100
+// time ./k23a -b siftsmall/siftsmall_base.fvecs -q siftsmall/siftsmall_query.fvecs -g siftsmall/siftsmall_groundtruth.ivecs -v vamana.bin -s new.bin -a 1.1 -l 150 -r 100
+
+void print_usage() {
+    std::cerr << "Usage: " << std::endl;
+    std::cerr << "---MANDATORY FLAGS---" << std::endl;
+    std::cerr << "-b <base file>" << std::endl;
+    std::cerr << "-q <query file>" << std::endl;
+    std::cerr << "-g <groundtruth file>" << std::endl;
+    std::cerr << "-a <alpha>" << std::endl;
+    std::cerr << "-l <l>" << std::endl;
+    std::cerr << "-r <r>" << std::endl;
+    std::cerr << "---OPTIONAL FLAGS---" << std::endl;
+    std::cerr << "-v <vamana file>" << std::endl;
+    std::cerr << "-s <save file>" << std::endl;
+    exit(EXIT_FAILURE);
+}
 
 // Parse input arguments and load necessary parameters
-void parse_parameters(int argc, char *argv[], std::string &base_file, std::string &query_file, std::string &groundtruth_file, std::string &vamana_file, float &a, int &L, int &R) {
+void parse_parameters(int argc, char *argv[], std::string &base_file, std::string &query_file, std::string &groundtruth_file, std::string &vamana_file, std::string &save_file, float &a, int &L, int &R) {
     int opt;
     bool base_flag = false, query_flag = false, ground_flag = false, a_flag = false, l_flag = false, r_flag = false;
     std::ifstream file;
 
-    // Require 13 arguments without vamana file, 15 with vamana file
-    if (argc != 13 && argc != 15) {
-        std::cerr << "Usages: " << std::endl \
-        << "- " << argv[0] << " -b <base file> -q <query file> -g <groundtruth file> -a <alpha> -l <L> -r <R>" << std::endl \
-        << "- " << argv[0] << " -b <base file> -q <query file> -g <groundtruth file> -v <vamana file> -a <alpha> -l <L> -r <R>";
-        exit(EXIT_FAILURE);
-    }
+    // Minimum arguements are 13 and maximum are 17. Arguements come in pairs so argc must always be odd
+    if (argc < 13 || argc > 17 || argc % 2 == 0) print_usage();
     
     // Parse arguments using getopt
-    while ((opt = getopt(argc, argv, "b:q:g:v:a:l:r:")) != -1) {
+    while ((opt = getopt(argc, argv, "b:q:g:v:s:a:l:r:")) != -1) {
         switch (opt) {
         case 'b': // Base vectors file
             base_file = optarg;
@@ -59,12 +70,15 @@ void parse_parameters(int argc, char *argv[], std::string &base_file, std::strin
                 exit(EXIT_FAILURE);
             }
             break;
-        case 'v': // Base vectors file
+        case 'v': // Vamana graph file
             vamana_file = optarg;
             if (!std::ifstream(vamana_file)) {
                 std::cerr << "Vamana file doesn't exist" << std::endl;
                 exit(EXIT_FAILURE);
             }
+            break;
+        case 's': // Save file
+            save_file = optarg;
             break;
         case 'a': // Alpha value
             a = std::stof(optarg);
@@ -91,20 +105,12 @@ void parse_parameters(int argc, char *argv[], std::string &base_file, std::strin
             }
             break;
         default:
-            std::cerr << "Usages: " << std::endl \
-            << "- " << argv[0] << " -b <base file> -q <query file> -g <groundtruth file> -a <alpha> -l <L> -r <R>" << std::endl \
-            << "- " << argv[0] << " -b <base file> -q <query file> -g <groundtruth file> -v <vamana file> -a <alpha> -l <L> -r <R>";
-            exit(EXIT_FAILURE);
+            print_usage();
         }
     }
 
     // Check if all mandatory flags are given
-    if (!base_flag || !query_flag || !ground_flag || !a_flag || !l_flag || !r_flag) {
-        std::cerr << "Usages: " << std::endl \
-        << "- " << argv[0] << " -b <base file> -q <query file> -g <groundtruth file> -a <alpha> -l <L> -r <R>" << std::endl \
-        << "- " << argv[0] << " -b <base file> -q <query file> -g <groundtruth file> -v <vamana file> -a <alpha> -l <L> -r <R>";
-        exit(EXIT_FAILURE);
-    }
+    if (!base_flag || !query_flag || !ground_flag || !a_flag || !l_flag || !r_flag) print_usage();
 
     // Output parameters
     std::cout << "-----Parameters-----" << std::endl;
@@ -112,6 +118,7 @@ void parse_parameters(int argc, char *argv[], std::string &base_file, std::strin
     std::cout << "Query file = " << query_file << std::endl;
     std::cout << "Groundtruth file = " << groundtruth_file << std::endl;
     std::cout << "Vamana file = " << vamana_file << std::endl;
+    std::cout << "Save file = " << save_file << std::endl;
     std::cout << "Vector dimension = " << VEC_DIMENSION << std::endl;
     std::cout << "L = " << L << std::endl;
     std::cout << "R = " << R << std::endl;
@@ -136,8 +143,8 @@ int main(int argc, char *argv[]) {
     // Initialize parameters and parse command line input
     int base_vectors = 10000, queries_vectors = 100, L, R;
     float a;
-    std::string base_file, query_file, groundtruth_file, vamana_file = "";
-    parse_parameters(argc, argv, base_file, query_file, groundtruth_file, vamana_file, a, L, R);
+    std::string base_file, query_file, groundtruth_file, vamana_file = "", save_file = "";
+    parse_parameters(argc, argv, base_file, query_file, groundtruth_file, vamana_file, save_file, a, L, R);
 
     // Load vectors
     int read_vectors;
@@ -156,9 +163,8 @@ int main(int argc, char *argv[]) {
         std::cout << "\n--- Menu ---\n"
                 << "1) Find K-nearest neighbors for a query\n"
                 << "2) Find K-nearest neighbors for all queries\n"
-                << "3) Save Vamana structure to file\n"
-                << "4) Exit\n"
-                << "Enter choice (1-4): ";
+                << "3) Exit\n"
+                << "Enter choice (1-3): ";
         std::cin >> choice;
 
         if (choice == 1) { // Single query processing
@@ -185,19 +191,7 @@ int main(int argc, char *argv[]) {
             }
             std::cout << "Recall: " << (static_cast<float>((K * queries_vectors) - mismatch_count) / (K * queries_vectors)) << std::endl;
 
-        } else if (choice == 3) { // Save Vamana structure to file
-            std::string filename;
-            std::cout << "Enter filename to save Vamana structure: ";
-            std::cin >> filename;
-
-            try {
-                write_vamana_to_file(*g, filename);;  
-                std::cout << "Vamana structure successfully saved to " << filename << std::endl;
-            } catch (const std::exception& e) {
-                std::cout << "Error saving Vamana structure: " << e.what() << std::endl;
-            }
-
-        } else if (choice == 4) { // Exit
+        } else if (choice == 3) { // Exit
             std::cout << "Exiting the program." << std::endl;
             break;
 
@@ -206,7 +200,8 @@ int main(int argc, char *argv[]) {
         }
     }
 
-
+    // Check if user wants to save the graph before deleting
+    if (!save_file.empty()) write_vamana_to_file(*g, save_file);
     delete g;
     return 0;
 }
