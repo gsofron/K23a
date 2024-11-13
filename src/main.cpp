3ea102fa069d754c267a30b9e8a81b3cd7d033d0
gzog23 <mix.raf.kos@gmail.com>
2024-11-13 02:09:00 +0200

    user can give an already constructed vamana graph using -v parameter

--- src/main.cpp
+++ src/main.cpp
@@ -14,25 +14,30 @@
 #include "vamana.hpp"
 #include "vectors.hpp"
 
-// Command line: time ./k23a -b siftsmall/siftsmall_base.fvecs -q siftsmall/siftsmall_query.fvecs -g siftsmall/siftsmall_groundtruth.ivecs -a 1.1 -l 150 -r 100
+// Execution examples
+// time ./k23a -b siftsmall/siftsmall_base.fvecs -q siftsmall/siftsmall_query.fvecs -g siftsmall/siftsmall_groundtruth.ivecs -a 1.1 -l 150 -r 100
+// time ./k23a -b siftsmall/siftsmall_base.fvecs -q siftsmall/siftsmall_query.fvecs -g siftsmall/siftsmall_groundtruth.ivecs -v vamana.bin -a 1.1 -l 150 -r 100
 
 // Parse input arguments and load necessary parameters
-void parse_parameters(int argc, char *argv[], std::string &base_file, std::string &query_file, 
-                      std::string &groundtruth_file, float &a, int &L, int &R) {
+void parse_parameters(int argc, char *argv[], std::string &base_file, std::string &query_file, std::string &groundtruth_file, std::string &vamana_file, float &a, int &L, int &R) {
     int opt;
+    bool base_flag = false, query_flag = false, ground_flag = false, a_flag = false, l_flag = false, r_flag = false;
     std::ifstream file;
 
-    // Require 13 arguments for full input specification
-    if (argc != 13) {
-        std::cerr << "Usage: " << argv[0] << " -b <base file> -q <query file> -g <groundtruth file> -a <alpha> -l <L> -r <R>" << std::endl;
+    // Require 13 arguments without vamana file, 15 with vamana file
+    if (argc != 13 && argc != 15) {
+        std::cerr << "Usages: " << std::endl \
+        << "- " << argv[0] << " -b <base file> -q <query file> -g <groundtruth file> -a <alpha> -l <L> -r <R>" << std::endl \
+        << "- " << argv[0] << " -b <base file> -q <query file> -g <groundtruth file> -v <vamana file> -a <alpha> -l <L> -r <R>";
         exit(EXIT_FAILURE);
     }
     
     // Parse arguments using getopt
-    while ((opt = getopt(argc, argv, "b:q:g:a:l:r:")) != -1) {
+    while ((opt = getopt(argc, argv, "b:q:g:v:a:l:r:")) != -1) {
         switch (opt) {
         case 'b': // Base vectors file
             base_file = optarg;
+            base_flag = true;
             if (!std::ifstream(base_file)) {
                 std::cerr << "Base vectors file doesn't exist" << std::endl;
                 exit(EXIT_FAILURE);
@@ -40,6 +45,7 @@ void parse_parameters(int argc, char *argv[], std::string &base_file, std::strin
             break;
         case 'q': // Query vectors file
             query_file = optarg;
+            query_flag = true;
             if (!std::ifstream(query_file)) {
                 std::cerr << "Query vectors file doesn't exist" << std::endl;
                 exit(EXIT_FAILURE);
@@ -47,13 +53,22 @@ void parse_parameters(int argc, char *argv[], std::string &base_file, std::strin
             break;
         case 'g': // Groundtruth vectors file
             groundtruth_file = optarg;
+            ground_flag = true;
             if (!std::ifstream(groundtruth_file)) {
                 std::cerr << "Groundtruth file doesn't exist" << std::endl;
                 exit(EXIT_FAILURE);
             }
             break;
+        case 'v': // Base vectors file
+            vamana_file = optarg;
+            if (!std::ifstream(vamana_file)) {
+                std::cerr << "Vamana file doesn't exist" << std::endl;
+                exit(EXIT_FAILURE);
+            }
+            break;
         case 'a': // Alpha value
             a = std::stof(optarg);
+            a_flag = true;
             if (a < 1.0) {
                 std::cerr << "Alpha value cannot be less than 1" << std::endl;
                 exit(EXIT_FAILURE);
@@ -61,6 +76,7 @@ void parse_parameters(int argc, char *argv[], std::string &base_file, std::strin
             break;
         case 'l': // Greedy search limit L
             L = std::stoi(optarg);
+            l_flag = true;
             if (L < K) {
                 std::cerr << "L cannot be smaller than K" << std::endl;
                 exit(EXIT_FAILURE);
@@ -68,16 +84,38 @@ void parse_parameters(int argc, char *argv[], std::string &base_file, std::strin
             break;
         case 'r': // Graph degree limit R
             R = std::stoi(optarg);
+            r_flag = true;
             if (R <= 0) {
                 std::cerr << "R must be positive" << std::endl;
                 exit(EXIT_FAILURE);
             }
             break;
         default:
-            std::cerr << "Usage: " << argv[0] << " -b <base file> -q <query file> -g <groundtruth file> -a <alpha> -l <L> -r <R>" << std::endl;
+            std::cerr << "Usages: " << std::endl \
+            << "- " << argv[0] << " -b <base file> -q <query file> -g <groundtruth file> -a <alpha> -l <L> -r <R>" << std::endl \
+            << "- " << argv[0] << " -b <base file> -q <query file> -g <groundtruth file> -v <vamana file> -a <alpha> -l <L> -r <R>";
             exit(EXIT_FAILURE);
         }
     }
+
+    // Check if all mandatory flags are given
+    if (!base_flag || !query_flag || !ground_flag || !a_flag || !l_flag || !r_flag) {
+        std::cerr << "Usages: " << std::endl \
+        << "- " << argv[0] << " -b <base file> -q <query file> -g <groundtruth file> -a <alpha> -l <L> -r <R>" << std::endl \
+        << "- " << argv[0] << " -b <base file> -q <query file> -g <groundtruth file> -v <vamana file> -a <alpha> -l <L> -r <R>";
+        exit(EXIT_FAILURE);
+    }
+
+    // Output parameters
+    std::cout << "-----Parameters-----" << std::endl;
+    std::cout << "Base file = " << base_file << std::endl;
+    std::cout << "Query file = " << query_file << std::endl;
+    std::cout << "Groundtruth file = " << groundtruth_file << std::endl;
+    std::cout << "Vamana file = " << vamana_file << std::endl;
+    std::cout << "Vector dimension = " << VEC_DIMENSION << std::endl;
+    std::cout << "L = " << L << std::endl;
+    std::cout << "R = " << R << std::endl;
+    std::cout << "a = " << a << std::endl;
 }
 
 // Helper function to find elements in `a` not in `b`
@@ -98,54 +136,21 @@ int main(int argc, char *argv[]) {
     // Initialize parameters and parse command line input
     int base_vectors = 10000, queries_vectors = 100, L, R;
     float a;
-    std::string base_file, query_file, groundtruth_file;
-    parse_parameters(argc, argv, base_file, query_file, groundtruth_file, a, L, R);
+    std::string base_file, query_file, groundtruth_file, vamana_file = "";
+    parse_parameters(argc, argv, base_file, query_file, groundtruth_file, vamana_file, a, L, R);
 
     // Load vectors
     int read_vectors;
     Vectors<float> vectors(base_file, read_vectors, base_vectors, queries_vectors);
     vectors.read_queries(query_file, queries_vectors);
 
-    // Output parameters
-    std::cout << "-----Parameters-----" << std::endl;
-    std::cout << "Base file = " << base_file << std::endl;
-    std::cout << "Query file = " << query_file << std::endl;
-    std::cout << "Groundtruth file = " << groundtruth_file << std::endl;
-    std::cout << "Base vectors = " << base_vectors << std::endl;
-    std::cout << "Queries vectors = " << queries_vectors << std::endl;
-    std::cout << "Vectors read = " << read_vectors << std::endl;
-    std::cout << "Vector dimension = " << VEC_DIMENSION << std::endl;
-    std::cout << "k = " << K << std::endl;
-    std::cout << "L = " << L << std::endl;
-    std::cout << "R = " << R << std::endl;
-    std::cout << "a = " << a << std::endl;
-
     DirectedGraph *g;
 
+    // If user gave vamana file, use it to initialize the graph
+    if (!vamana_file.empty()) g = read_vamana_from_file(vamana_file);
+    else g = vamana(vectors, a, L, R);
+
     int choice;
-    
-    while (true) {
-        std::cout << "\n--- Vamana Initialization ---\n"
-                  << "1) Read Vamana structure from file\n"
-                  << "2) Create a new Vamana structure\n"
-                  << "Enter choice (1-2): ";
-        std::cin >> choice;
-
-        if (choice == 1) { // Option to read Vamana from file
-            std::string filename;
-            std::cout << "Enter filename to read Vamana structure: ";
-            std::cin >> filename;
-
-            g = read_vamana_from_file(filename);
-            break;
-        } else if (choice == 2) { // Option to create a new Vamana structure
-            std::cout << "Creating Vamana..." << std::endl;
-            g = vamana(vectors, a, L, R);
-            break;
-        } else {
-            std::cout << "Invalid option. Please enter 1, 2." << std::endl;
-        }
-    }
 
     while (true) {
         std::cout << "\n--- Menu ---\n"
