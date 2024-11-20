bbfac14d82dd558aa7cd1e14b45f48f5c55d45de
gsofron <giorgos.sofronas03@gmail.com>
2024-11-20 13:02:38 +0200

    init findmedoid_test.cpp

---
+++ tests/findmedoid_test.cpp
@@ -0,0 +1,11 @@
+#include "acutest.h"
+#include "findmedoid.hpp"
+
+void test_find_medoid(void) {
+    TEST_CHECK(1);
+}
+
+TEST_LIST = {
+    { "test_find_medoid", test_find_medoid },
+    { NULL, NULL }
+};
