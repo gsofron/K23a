654c7a6053d32e0d272e9da04b8317950dd142db
gzog23 <mix.raf.kos@gmail.com>
2024-11-21 18:07:30 +0200

    parse base vectors and query vectors number

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,4 @@
-#define VEC_DIMENSION 128
+#define VEC_DIMENSION 100
 #define K 100
 
 #include <cstdlib>     
@@ -15,9 +15,9 @@
 #include "vectors.hpp"
 
 // Execution examples
-// time ./k23a -b siftsmall/siftsmall_base.fvecs -q siftsmall/siftsmall_query.fvecs -g siftsmall/siftsmall_groundtruth.ivecs -a 1.1 -l 150 -r 100 -t 50 -i -1
-// time ./k23a -b siftsmall/siftsmall_base.fvecs -q siftsmall/siftsmall_query.fvecs -g siftsmall/siftsmall_groundtruth.ivecs -s vamana.bin -a 1.1 -l 150 -r 100 -t 50 -i -1
-// time ./k23a -b siftsmall/siftsmall_base.fvecs -q siftsmall/siftsmall_query.fvecs -g siftsmall/siftsmall_groundtruth.ivecs -v vamana.bin -s new.bin -a 1.1 -l 150 -r 100 -t 50 -i -1
+// time ./k23a -b dummy/dummy-data.bin -q dummy/dummy-queries.bin -g dummy/dummy-groundtruth.bin -n 10000 -m 10000 -a 1.1 -l 150 -r 100 -t 50 -i -1
+// time ./k23a -b dummy/dummy-data.bin -q dummy/dummy-queries.bin -g dummy/dummy-groundtruth.bin -s vamana.bin -n 10000 -m 10000 -a 1.1 -l 150 -r 100 -t 50 -i -1
+// time ./k23a -b dummy/dummy-data.bin -q dummy/dummy-queries.bin -g dummy/dummy-groundtruth.bin -v vamana.bin -s new.bin -n 10000 -m 10000 -a 1.1 -l 150 -r 100 -t 50 -i -1
 
 void print_usage() {
     std::cerr << "Usage: " << std::endl;
@@ -25,11 +25,13 @@ void print_usage() {
     std::cerr << "-b <base file>" << std::endl;
     std::cerr << "-q <query file>" << std::endl;
     std::cerr << "-g <groundtruth file>" << std::endl;
+    std::cerr << "-n <base vectors number>" << std::endl;
+    std::cerr << "-m <query vectors number>" << std::endl;
     std::cerr << "-a <alpha>" << std::endl;
     std::cerr << "-l <l>" << std::endl;
     std::cerr << "-r <r>" << std::endl;
     std::cerr << "-t <tau>" << std::endl;
-    std::cerr << "-i <index> (use -1 to calculate total recall)" << std::endl;
+    std::cerr << "-i <query index> (use -1 to calculate total recall)" << std::endl;
     std::cerr << "---OPTIONAL FLAGS---" << std::endl;
     std::cerr << "-v <vamana file>" << std::endl;
     std::cerr << "-s <save file>" << std::endl;
@@ -37,16 +39,19 @@ void print_usage() {
 }
 
 // Parse input arguments and load necessary parameters
-void parse_parameters(int argc, char *argv[], int query_count, std::string &base_file, std::string &query_file, std::string &groundtruth_file, std::string &vamana_file, std::string &save_file, float &a, int &L, int &R, int &t, int &index) {
+void parse_parameters(int argc, char *argv[], std::string &base_file, std::string &query_file, std::string &groundtruth_file, \
+                        std::string &vamana_file, std::string &save_file, int &base_vectors_num, int &query_vectors_num, \
+                        float &a, int &L, int &R, int &t, int &index) {
     int opt;
-    bool base_flag = false, query_flag = false, ground_flag = false, a_flag = false, l_flag = false, r_flag = false, index_flag = false, t_flag = false;
+    bool base_flag = false, query_flag = false, ground_flag = false, base_num_flag = false, query_num_flag = false, \
+        a_flag = false, l_flag = false, r_flag = false, index_flag = false, t_flag = false;
     std::ifstream file;
 
-    // Minimum arguements are 17 and maximum are 21. Arguements come in pairs so argc must always be odd
-    if (argc < 17 || argc > 21 || argc % 2 == 0) print_usage();
+    // Minimum arguements are 21 and maximum are 25. Arguements come in pairs so argc must always be odd
+    if (argc < 21 || argc > 25 || argc % 2 == 0) print_usage();
     
     // Parse arguments using getopt
-    while ((opt = getopt(argc, argv, "b:q:g:v:s:a:l:r:t:i:")) != -1) {
+    while ((opt = getopt(argc, argv, "b:q:g:v:s:n:m:a:l:r:t:i:")) != -1) {
         switch (opt) {
         case 'b': // Base vectors file
             base_file = optarg;
@@ -81,6 +86,22 @@ void parse_parameters(int argc, char *argv[], int query_count, std::string &base
             break;
         case 's': // Save file
             save_file = optarg;
+            break;
+        case 'n': // Number of base vectors
+            base_vectors_num = std::stoi(optarg);
+            base_num_flag = true;
+            if (base_vectors_num <= 0) {
+                std::cerr << "Base vectors number must be positive" << std::endl;
+                exit(EXIT_FAILURE);
+            }
+            break;
+        case 'm': // Number of query vectors
+            query_vectors_num = std::stoi(optarg);
+            query_num_flag = true;
+            if (query_vectors_num <= 0) {
+                std::cerr << "Query vectors number must be positive" << std::endl;
+                exit(EXIT_FAILURE);
+            }
             break;
         case 'a': // Alpha value
             a = std::stof(optarg);
@@ -117,18 +138,20 @@ void parse_parameters(int argc, char *argv[], int query_count, std::string &base
         case 'i': // Index of query vector
             index = std::stoi(optarg);
             index_flag = true;
-            if (index < -1 || index >= query_count) {
-                std::cerr << "Invalid index" << std::endl;
-                exit(EXIT_FAILURE);
-            }
             break;
         default:
             print_usage();
         }
     }
 
+    // Check this here to make sure query_vectors_num is initialized
+    if (index < -1 || index >= query_vectors_num) {
+        std::cerr << "Invalid query index" << std::endl;
+        exit(EXIT_FAILURE);
+    }
+
     // Check if all mandatory flags are given
-    if (!base_flag || !query_flag || !ground_flag || !a_flag || !l_flag || !r_flag || !index_flag || !t_flag) print_usage();
+    if (!base_flag || !query_flag || !ground_flag || !base_num_flag || !query_num_flag || !a_flag || !l_flag || !r_flag || !index_flag || !t_flag) print_usage();
 
     // Output parameters
     std::cout << "-----Parameters-----" << std::endl;
@@ -138,9 +161,12 @@ void parse_parameters(int argc, char *argv[], int query_count, std::string &base
     std::cout << "Vamana file = " << vamana_file << std::endl;
     std::cout << "Save file = " << save_file << std::endl;
     std::cout << "Vector dimension = " << VEC_DIMENSION << std::endl;
+    std::cout << "k = " << K << std::endl;
+    std::cout << "Base vectors number = " << base_vectors_num << std::endl;
+    std::cout << "Query vectors number = " << query_vectors_num << std::endl;
+    std::cout << "a = " << a << std::endl;
     std::cout << "L = " << L << std::endl;
     std::cout << "R = " << R << std::endl;
-    std::cout << "a = " << a << std::endl;
     std::cout << "t = " << t << std::endl;
     std::cout << "index = " << index << std::endl;
     std::cout << std::endl;
@@ -162,44 +188,44 @@ int main(int argc, char *argv[]) {
     srand(time(NULL));  // Seed for randomization
 
     // Initialize parameters and parse command line input
-    int base_vectors = 10000, queries_vectors = 100, L, R, t, index;
+    int base_vectors_num, query_vectors_num, L, R, t, index;
     float a;
     std::string base_file, query_file, groundtruth_file, vamana_file = "", save_file = "";
-    parse_parameters(argc, argv, queries_vectors, base_file, query_file, groundtruth_file, vamana_file, save_file, a, L, R, t, index);
+    parse_parameters(argc, argv, base_file, query_file, groundtruth_file, vamana_file, save_file, base_vectors_num, query_vectors_num, a, L, R, t, index);
 
-    // Load vectors
-    int read_vectors;
-    Vectors<float> vectors(base_file, read_vectors, base_vectors, queries_vectors);
-    vectors.read_queries(query_file, queries_vectors);
+    // // Load vectors
+    // int read_vectors;
+    // Vectors<float> vectors(base_file, read_vectors, base_vectors, queries_vectors);
+    // vectors.read_queries(query_file, queries_vectors);
 
-    DirectedGraph *g;
+    // DirectedGraph *g;
 
-    // If user gave vamana file, use it to initialize the graph
-    if (!vamana_file.empty()) g = read_vamana_from_file(vamana_file);
-    else g = vamana(vectors, a, L, R);
+    // // If user gave vamana file, use it to initialize the graph
+    // if (!vamana_file.empty()) g = read_vamana_from_file(vamana_file);
+    // else g = vamana(vectors, a, L, R);
 
-    // User wants to calculate total recall
-    if (index == -1) {
-        int mismatch_count = 0;
-        for (int j = 0; j < queries_vectors; j++) {
-            auto result = GreedySearch(*g, vectors, 0, j + base_vectors, K, L);
-            auto groundtruth = vectors.query_solutions(groundtruth_file, j);
-            mismatch_count += findDifference(groundtruth, result.first).size();
-        }
-        std::cout << "Recall: " << (static_cast<float>((K * queries_vectors) - mismatch_count) / (K * queries_vectors)) << std::endl;
-    }
-    // User wants to calculate a single recall
-    // NOTE: Since we check if index is valid during parse_parameters(), we are always in bounds for GreedySearch()
-    else {
-        auto result = GreedySearch(*g, vectors, 0, index + base_vectors, K, L);
-        auto groundtruth = vectors.query_solutions(groundtruth_file, index);
-        std::vector<int> difference = findDifference(groundtruth, result.first);
-        std::cout << "Recall: " << (static_cast<float>(K - difference.size()) / K) << std::endl;
-    }
+    // // User wants to calculate total recall
+    // if (index == -1) {
+    //     int mismatch_count = 0;
+    //     for (int j = 0; j < queries_vectors; j++) {
+    //         auto result = GreedySearch(*g, vectors, 0, j + base_vectors, K, L);
+    //         auto groundtruth = vectors.query_solutions(groundtruth_file, j);
+    //         mismatch_count += findDifference(groundtruth, result.first).size();
+    //     }
+    //     std::cout << "Recall: " << (static_cast<float>((K * queries_vectors) - mismatch_count) / (K * queries_vectors)) << std::endl;
+    // }
+    // // User wants to calculate a single recall
+    // // NOTE: Since we check if index is valid during parse_parameters(), we are always in bounds for GreedySearch()
+    // else {
+    //     auto result = GreedySearch(*g, vectors, 0, index + base_vectors, K, L);
+    //     auto groundtruth = vectors.query_solutions(groundtruth_file, index);
+    //     std::vector<int> difference = findDifference(groundtruth, result.first);
+    //     std::cout << "Recall: " << (static_cast<float>(K - difference.size()) / K) << std::endl;
+    // }
 
-    // Check if user wants to save the graph
-    if (!save_file.empty()) write_vamana_to_file(*g, save_file);
+    // // Check if user wants to save the graph
+    // if (!save_file.empty()) write_vamana_to_file(*g, save_file);
 
-    delete g;
+    // delete g;
     return 0;
 }
