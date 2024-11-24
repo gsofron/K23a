ca10f9cbe22745ae29a6b0494a9790d2feccee1d
gsofron <giorgos.sofronas03@gmail.com>
2024-11-24 12:35:03 +0200

    filtered vamana init

---
+++ include/filtered_vamana.hpp
@@ -0,0 +1,5 @@
+#pragma once
+
+#include "directed_graph.hpp"
+
+DirectedGraph *filtered_vamana(Vectors& P, float a, int L, int R, int threshold);
---
+++ tests/filtered_vamana_test.cpp
@@ -0,0 +1,11 @@
+#include "acutest.h"
+#include "filtered_vamana.hpp"
+
+void test_filtered_vamana(void) {
+    TEST_CHECK(1);
+}
+
+TEST_LIST = {
+    { "test_filtered_vamana", test_filtered_vamana },
+    { NULL, NULL }
+};
