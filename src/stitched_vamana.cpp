f947943ec4b1b94d9350a0e6f4551632f3b1554c
sdi2100184 <st.tziouma@gmail.com>
2024-11-26 15:10:05 +0200

    a good start/ logic ready

---
+++ src/stitched_vamana.cpp
@@ -0,0 +1,28 @@
+#include "stitched_vamana.hpp"
+#include "filtered_robust_prune.hpp"
+#include "vamana.hpp"
+
+DirectedGraph *stitched_vamana(Vectors& P, float a, int L_small, int R_small, int R_stitched) {
+    int n = P.size();
+    // Initialize G to an empty graph
+    DirectedGraph *G = new DirectedGraph(n);
+
+    // For each filter find the corresponding graph and stitch them together
+    for (const auto& pair : P.filters_map) {
+        DirectedGraph *G_f = vamana(P, a, L_small, R_small);
+        G->stitch(G_f);
+    }
+
+    // For each vertex call the filtered robust prune
+    int size = P.size();
+    for (int i = 0 ; i < size ; i++) {
+        std::unordered_set<int> neighbors_i = G->get_neighbors(i);
+
+        std::set<std::pair<float, int>> ordered_neighbors_i;
+        for (auto v : neighbors_i) {
+            ordered_neighbors_i.insert({P.euclidean_distance_cached(i, v), v});
+        }
+
+        filtered_robust_prune(G, P, i, ordered_neighbors_i, a, R_stitched);
+    }
+}
