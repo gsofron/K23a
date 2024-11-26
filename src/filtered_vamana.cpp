3be7e43d98e95166459b1e48eb2862cb118361f3
gsofron <giorgos.sofronas03@gmail.com>
2024-11-26 11:08:21 +0200

    filtered vamana test + improvements

--- src/filtered_vamana.cpp
+++ src/filtered_vamana.cpp
@@ -16,9 +16,8 @@ DirectedGraph *filtered_vamana(Vectors& P, float a, int L, int R, int threshold)
 
     // Create the random permutation sigma (σ)
     int *sigma = new int[n];
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < n; i++)
         sigma[i] = i;
-    }
     // Shuffle to create the random permutation
     // Source for how to shuffle: https://stackoverflow.com/a/6926473
     auto rd = std::random_device {}; 
@@ -26,11 +25,10 @@ DirectedGraph *filtered_vamana(Vectors& P, float a, int L, int R, int threshold)
     std::shuffle(sigma, sigma + n, rng);
 
     for (int i = 0; i < n; i++) {
-        float Fx = P.filters[sigma[i]];
-        int s = st->at(Fx);
+        float Fx = P.filters[sigma[i]]; // Filter of current index
+        int s = st->at(Fx); // Medoid point of this filter
 
         auto V_Fx = FilteredGreedySearch(*G, P, s, sigma[i], 0, L).second;
-
         filtered_robust_prune(G, P, sigma[i], V_Fx, a, R);
 
         const auto& N_out_sigma_i = G->get_neighbors(sigma[i]);
@@ -38,8 +36,7 @@ DirectedGraph *filtered_vamana(Vectors& P, float a, int L, int R, int threshold)
             G->insert(j, sigma[i]);
             const auto& N_out_j = G->get_neighbors(j);
 
-            if (N_out_j.size() > R) {
-
+            if ((int)N_out_j.size() > R) {
                 std::set<std::pair<float, int>> new_N_out_j;
                 for (auto v : N_out_j)
                     new_N_out_j.insert({P.euclidean_distance_cached(j, v), v});
@@ -50,7 +47,7 @@ DirectedGraph *filtered_vamana(Vectors& P, float a, int L, int R, int threshold)
     }
 
     delete[] sigma;
-    delete[] st;
+    delete st;
 
     return G;
 }
